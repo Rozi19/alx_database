973a22b8da5ec23cf8aab5105800743afcf77afc
Roza Alene <rozaalene@gmail.com>
2023-08-16 03:29:16 -0700

    add * for tables

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,7 +2,7 @@
 -- set password and give privilage
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-SET PASSWORD FOR = "user_0d_2_pwd";
+SET PASSWORD FOR = 'user_0d_2_pwd';
 GRANT SELECT
-ON hbtn_0d_2
+ON 'hbtn_0d_2'.*
 TO 'user_0d_2'@'localhost';
